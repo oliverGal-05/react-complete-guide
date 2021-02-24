d6fb56fdf1f782e815aa098e446342c71e854ff4
Olic <galoliver05@gmail.com>
2021-02-24 14:07:50 +0100

    Component & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Let's GO!</h1>
+        <Person name="Max">My hobbie is: Ice-Skating</Person>
+        <Person name="John"/>
       </div>
     );
   }
